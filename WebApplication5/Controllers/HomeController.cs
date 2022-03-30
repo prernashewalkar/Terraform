9cc529ed7385bceceed6c3abfd04afce447a8db2
Prerana Shewalkar <preranashewalkar@hotmail.com>
2022-03-30 15:37:21 +0530

    changed

--- WebApplication5/Controllers/HomeController.cs
+++ WebApplication5/Controllers/HomeController.cs
@@ -15,7 +15,7 @@ namespace WebApplication5.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = "Prerna Your application description page.";
 
             return View();
         }
